13e1e2eaaccbe6e0eee0d18352ab5b79ae612059
dongchancho <deamp@naver.com>
2023-12-18 22:58:12 +0900

    두번째 커밋

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import ExpenseItem from "./components/ExpenseItem";
+//사용자 지정 컴포넌트는 대문자로 시작되는것을 규칙으로 함.
 function App() {
+  //명령적 접근법
+  const para = document.createElement('p');
+  para.textContent = 'This is also visible';
+  document.getElementById('root').append(para);
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
+
+//JSX란
+/** html = xml
+ * JavaScript XML
+ */
 
 export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,8 @@
+//리액트의 컴포넌트는 단지 자바스크립트의 함수에 불가함.
+//반환되는게 JSX라는게 특이하지만 js fn이라는게 핵심.
+
+function ExpenseItem(){
+    return <h2>Expense item!</h2>
+}
+
+export default ExpenseItem;
